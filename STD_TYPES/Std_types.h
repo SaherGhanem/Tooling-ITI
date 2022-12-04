95770d3ebb3774460b613e014203219426f45729
Saher Ghanem <saherghanem333@gmail.com>
2022-12-04 07:11:54 +0200

    ALL Standard Types

---
+++ STD_TYPES/Std_types.h
@@ -0,0 +1,55 @@
+/****************************************************************************/
+/****************************************************************************/
+/**************************		Author: SAHER GHANEM 	*********************/
+/**************************		   ITI-INTAKE43         *********************/
+/**************************		Layer:  Libraries		*********************/
+/**************************		SWC:	STD_TYPES		*********************/
+/**************************		   Version:1.00 	    *********************/
+/**************************		DATE: 11/28/2022       	*********************/
+/****************************************************************************/
+/****************************************************************************/
+
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+typedef unsigned char u8;              // unsigned char 			         1 Byte
+typedef unsigned char* pu8;            // pointer to unsigned char 	         4 Byte
+
+typedef unsigned short int u16;	       // unsigned short int                 2 Byte
+typedef unsigned short int* pu16;	   // pointer to unsigned short int      4 Byte
+
+typedef unsigned long  int u32;	       // unsigned long  int                 4 Byte
+typedef unsigned long  int* pu32;	   // pointer to unsigned long  int      4 Byte
+
+typedef unsigned long long int u64;    // unsigned long long int             8 Byte
+typedef unsigned long long int* pu64;  // pointer to unsigned long long int  4 Byte
+
+
+
+
+typedef signed char s8;				  // signed char			             1 Byte
+typedef signed char* ps8;			  // pointer to signed char			     4 Byte
+
+typedef signed short int s16;         // signed short int		             2 Byte
+typedef signed short int* ps16;       // pointer to signed short int		 4 Byte
+
+typedef signed long int s32;          // signed long  int		             4 Byte
+typedef signed long int* ps32;        // pointer to signed long int		     4 Byte
+
+typedef signed long long int s64;     // signed long long int                8 Byte
+typedef signed long long int* ps64;   // pointer to signed long long int     4 Byte
+
+
+
+
+typedef float  f32;			          // float				                 4 Byte
+typedef float*  pf32;			      // pointer to float			         4 Byte
+
+typedef double f64;                   // double				                 8 Byte
+typedef double* pf64;                 // pointer to double		             4 Byte
+
+typedef long double f96;              // long double			             12 Byte
+typedef long double* pf96;            // pointer to long double			     4  Byte
+
+
+#endif
